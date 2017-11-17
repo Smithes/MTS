15af351bace22a977377bef58142e7268427c379
Mark Steven Buder <2893338+Smithes@users.noreply.github.com>
2017-11-17 12:24:14 +0100

    Update environment.prod.ts

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { BackendType } from '../app/config';
 export const environment: {production: boolean, backendType: BackendType, restPathRoot: string, restServiceRoot: string} = {
   production: true,
   backendType: BackendType.REST,
-  restPathRoot: 'http://de-muc-mythai:9090/mythaistar/',
-  restServiceRoot: 'http://de-muc-mythai:9090/mythaistar/services/rest/',
+  restPathRoot: 'http://localhost:9090/mythaistar/',
+  restServiceRoot: 'http://localhost:9090/mythaistar/services/rest/',
 };
